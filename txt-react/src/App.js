fe54303b669c8aba1a6ef226617a52054e96b970
hyylft <rdsht520@163.com>
2018-08-10 14:22:11 +0800

    eduit

--- txt-react/src/App.js
+++ txt-react/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-// import Header from './component/header/Header';
-// import ItemList from './component/itemlist/ItemList';
-// import Footer from './component/footer/Footer';
-import ProvinceSelect from './unit/province/ProvinceSelect';
-// import './App.css';
+import Header from './component/header/Header';
+import ItemList from './component/itemlist/ItemList';
+import Footer from './component/footer/Footer';
+import './App.css';
 
 class App extends React.Component {
   static defaultProps = {
